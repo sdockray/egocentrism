21489a56353dc5f49273c81838d4b9b154b4ac04
sdockray <sean.patrick.dockray@gmail.com>
2026-08-09 18:06:35 +1000

    export partial map

--- src/sonic/export_partial_map_data.py
+++ src/sonic/export_partial_map_data.py
@@ -24,7 +24,8 @@ from src.sonic.reduction import reduce_mfccs_to_2d
 
 WORKSPACE_ROOT = Path(__file__).resolve().parents[2]
 DEFAULT_ASR_DIR = Path("/app/2026/audio_cache/asr")
-DEFAULT_OUTPUT = WORKSPACE_ROOT / "web" / "sonic_map" / "map_data.json"
+#DEFAULT_OUTPUT = WORKSPACE_ROOT / "web" / "sonic_map" / "map_data.json"
+DEFAULT_OUTPUT = Path("/app/2026/web/sonic_map") / "map_data.json"
 DEFAULT_CACHE_DIR = Path("/app/2026/audio_cache/partial_map_cache")
 CACHE_SCHEMA_VERSION = 2
 
